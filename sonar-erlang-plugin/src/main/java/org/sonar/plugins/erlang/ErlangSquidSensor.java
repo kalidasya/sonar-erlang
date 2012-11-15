50ad414921c0e5e5adcb689f3e36031d9598b565
spil-tamas <tamas.kende@spilgames.com>
2012-11-15 10:19:30 +0100

    fix maven build (animal sniffer, license check)

--- sonar-erlang-plugin/src/main/java/org/sonar/plugins/erlang/ErlangSquidSensor.java
+++ sonar-erlang-plugin/src/main/java/org/sonar/plugins/erlang/ErlangSquidSensor.java
@@ -1,3 +1,22 @@
+/*
+ * Sonar Erlang Plugin
+ * Copyright (C) 2012 Tamas Kende
+ * kende.tamas@gmail.com
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
 package org.sonar.plugins.erlang;
 
 import java.util.Collection;
@@ -51,7 +70,7 @@ public class ErlangSquidSensor implements Sensor {
   }
 
   public boolean shouldExecuteOnProject(Project project) {
-    return Erlang.KEY.equals(project.getLanguageKey());
+    return Erlang.KEY.equals(project.getLanguage().getKey());
   }
 
   public void analyse(Project project, SensorContext context) {
--- sonar-erlang-plugin/src/test/java/org/sonar/plugins/erlang/ErlangSquidSensorTest.java
+++ sonar-erlang-plugin/src/test/java/org/sonar/plugins/erlang/ErlangSquidSensorTest.java
@@ -1,8 +1,28 @@
+/*
+ * Sonar Erlang Plugin
+ * Copyright (C) 2012 Tamas Kende
+ * kende.tamas@gmail.com
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
 package org.sonar.plugins.erlang;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.fest.assertions.Assertions.assertThat;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -17,6 +37,7 @@ import org.sonar.api.measures.FileLinesContextFactory;
 import org.sonar.api.profiles.RulesProfile;
 import org.sonar.api.resources.InputFile;
 import org.sonar.api.resources.InputFileUtils;
+import org.sonar.api.resources.Language;
 import org.sonar.api.resources.Project;
 import org.sonar.api.resources.ProjectFileSystem;
 import org.sonar.api.resources.Resource;
@@ -37,14 +58,20 @@ public class ErlangSquidSensorTest {
 		sensor = new ErlangSquidSensor(mock(RulesProfile.class), fileLinesContextFactory);
 	}
 
-	/*@Test
+	@Test
 	public void should_execute_on_erlang_project() {
 		Project project = new Project("key");
-		project.setLanguageKey("java");
+		
+		Language java = mock(Language.class);
+		when(java.getKey()).thenReturn("java");
+		project.setLanguage(java);
 		assertThat(sensor.shouldExecuteOnProject(project)).isFalse();
-		project.setLanguageKey("erl");
+		
+		Language erl = mock(Language.class);
+		when(erl.getKey()).thenReturn("erl");
+		project.setLanguage(erl);
 		assertThat(sensor.shouldExecuteOnProject(project)).isTrue();
-	}*/
+	}
 
 	@Test
 	public void should_analyse() {
