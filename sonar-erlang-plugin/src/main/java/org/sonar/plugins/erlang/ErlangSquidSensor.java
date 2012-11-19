5cb83a0b6497b830345dbd1cb37dd304d083d7e3
spil-tamas <tamas.kende@spilgames.com>
2012-11-19 13:08:02 +0100

    add missing metrics

--- sonar-erlang-plugin/src/main/java/org/sonar/plugins/erlang/ErlangSquidSensor.java
+++ sonar-erlang-plugin/src/main/java/org/sonar/plugins/erlang/ErlangSquidSensor.java
@@ -119,6 +119,17 @@ public class ErlangSquidSensor implements Sensor {
 				.getDouble(ErlangMetric.COMPLEXITY));
 		context.saveMeasure(sonarFile, CoreMetrics.COMMENT_LINES, squidFile
 				.getDouble(ErlangMetric.COMMENT_LINES));
+		context.saveMeasure(sonarFile, CoreMetrics.COMMENT_LINES_DENSITY, squidFile
+				.getDouble(ErlangMetric.COMMENT_LINES)/squidFile
+				.getDouble(ErlangMetric.LINES_OF_CODE));
+		context.saveMeasure(sonarFile, CoreMetrics.PUBLIC_API, squidFile
+				.getDouble(ErlangMetric.PUBLIC_API));
+		context.saveMeasure(sonarFile, CoreMetrics.PUBLIC_UNDOCUMENTED_API, squidFile
+				.getDouble(ErlangMetric.PUBLIC_API)-squidFile
+				.getDouble(ErlangMetric.PUBLIC_DOC_API));
+		context.saveMeasure(sonarFile, CoreMetrics.PUBLIC_DOCUMENTED_API_DENSITY, squidFile
+				.getDouble(ErlangMetric.PUBLIC_DOCUMENTED_API_DENSITY));
+		
 	}
 
 	private void saveFunctionsComplexityDistribution(File sonarFile, SourceFile squidFile) {
